2bf9a00e0ada74936a4d2ee92f8674b2fdfd7dfb
vanessalopeznr <vanessalopeznr@gmail.com>
2024-02-02 17:10:14 +0100

    Upload

--- TPS/tp4_b.cpp
+++ TPS/tp4_b.cpp
@@ -2,5 +2,25 @@
 #include "Mutex.h"
 
 Mutex::Mutex(){
-    
+    pthread_mutex_init(posixId, nullptr);
+}
+
+Mutex::~Mutex(){
+     pthread_mutex_destroy(posixId);
+}
+
+void Mutex::lock(){
+
+}
+
+bool Mutex::lock(double timeout_ms){
+
+}
+
+bool Mutex::trylock(){
+
+}
+
+void Mutex::unlock(){
+
 }
