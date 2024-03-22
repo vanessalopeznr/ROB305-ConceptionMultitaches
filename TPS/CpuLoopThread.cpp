405153c2dde37ec0c41931f29a3800ace84afd6f
vanessalopeznr <vanessalopeznr@gmail.com>
2024-03-23 00:22:26 +0100

    Upload

--- TPS/CpuLoopThread.cpp
+++
@@ -1,9 +0,0 @@
-#include "CpuLoopThread.h"
-#include "CpuLoop.h"
-#include "Mutex.h"
-#include "Calibrator.h"
-#include <time.h>
-
-CpuLoopThread::CpuLoopThread(Mutex& mutex, CpuLoop& cpuLoop):Thread(), mutex(mutex), cpuLoop(cpuLoop) {}
-
-CpuLoopThread::~CpuLoopThread() {}
